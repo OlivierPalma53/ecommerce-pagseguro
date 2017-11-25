1ba954d4c573e6ca2d249ecc5c62c907ede1cbe3
Olivier Palma <olivierpalmasouza@gmail.com>
2017-11-24 23:18:01 -0200

    Criando a classe de configuração para o pagseguro

---
+++ vendor/hcodebr/php-classes/src/PagSeguro/Config.php
@@ -0,0 +1,37 @@
+<?php
+
+  namespace Hcode\PagSeguro;
+
+  class Config{
+
+    const SANDBOX = true;
+
+    const SAND_EMAIL = "olivierpalmasouza@gmail.com";
+    const PRODUCTION_EMAIL = "olivierpalmasouza@gmail.com";
+
+    const SAND_TOKEN = "57F3D2761811428CB7DE07273EF0CD7B";
+    const PRODUCTION_TOKEN = "A7322953972842139645D5AF797F0948";
+
+    const SANDBOX_SESSIONS = "https://ws.sandbox.pagseguro.uol.com.br/v2/sessions";
+    const PRODUCTION_SESSIONS = "https://ws.pagseguro.uol.com.br/v2/sessions";
+
+    public static function getAuthentication():array{
+
+      if(Config::SANDBOX === true){
+        return [
+          'email' => Config::SAND_EMAIL,
+          'token' => Config::SAND_TOKEN
+        ];
+      } else {
+        return [
+          'email' => Config::PRODUCTION_EMAIL,
+          'token' => Config::PRODUCTION_TOKEN
+        ];
+      }
+
+    }
+
+    public static function getUrlSessions():String{
+      return (Config::SANDBOX === true) ? Config::SANDBOX_SESSIONS : Config::PRODUCTION_SESSIONS;
+    }
+  }
